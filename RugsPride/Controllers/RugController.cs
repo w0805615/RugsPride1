f553b40ece98c339c3379e721fbb116178a429c2
Manvir Kaur Brar <W0805615@myscc.ca>
2023-02-15 17:03:22 -0500

    Added new controller named: RugController.cs

---
+++ RugsPride/Controllers/RugController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace RugsPride.Controllers{
+    public class RugController : Controller
+    {
+        // 
+        // GET: /Rug/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /Rug/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
--- RugsPride/README.txt
+++ RugsPride/README.txt
@@ -9,3 +9,9 @@ Date Created: 2023-02-15 2130UTC
 
 Created Welcome page
 Created Git Repository
+
+Completed Part 1 of tutorial 
+
+455 ET
+Added a new controller : RugController
+Changed the content of controller based on the tutorial 
