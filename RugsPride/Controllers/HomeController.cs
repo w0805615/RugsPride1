df545120ce8457dd997f3774f290badd5a39de30
Manvir Kaur Brar <w0805615@myscc.ca>
2023-02-17 12:03:17 -0500

    Added css and images Created About page

--- RugsPride/Controllers/HomeController.cs
+++ RugsPride/Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace RugsPride.Controllers
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
